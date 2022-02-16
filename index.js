b073351dabf1d8f283070624aa6e0971c7226a5e
dawon0304 <dawon0304@naver.com>
2022-02-17 00:00:16 +0900

    플레이버튼 변환, 배경음악 재생

--- index.js
+++ index.js
@@ -1,4 +1,53 @@
-window.addEventListener("load", function(event) {
-  this.alert("HEllo!!")
-});
 
+// 배경음악 play/ pause기능
+const bgaudio = new Audio();
+bgaudio.src = "./src/sound/bg.mp3";
+
+  function playBg() {
+  bgaudio.play();
+}
+
+  function pauseBg() {
+    bgaudio.pause();
+  }
+
+  // 결과박스 
+
+
+
+
+
+
+
+
+
+
+// var timeInSecs;
+// var ticker;
+
+// function startTimer(secs) {
+// timeInSecs = parseInt(secs);
+// ticker = setInterval("tick()", 1000); 
+// }
+
+// function tick( ) {
+// var secs = timeInSecs;
+// if (secs > 0) {
+// timeInSecs--; 
+// }
+// else {
+// clearInterval(ticker);
+// startTimer(1*10); //10seconds
+// }
+
+// var mins = Math.floor(secs/60);
+// secs %= 60;
+// var pretty = ( (mins < 10) ? "0" : "" ) + mins + ":" + ( (secs < 10) ? "0" : "" ) + secs;
+
+// document.getElementById("seconds").innerHTML = pretty;
+// }
+
+// startTimer(1*10); //10seconds
+
+
+//Credits to Gulzaib from Pakistan
